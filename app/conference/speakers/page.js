055efc1f563c22ebb47c229274e61c17ed7f96ee
Adhithi Ravichandran <adhithi@redivus.com>
2022-12-01 11:25:26 -0600

    Adding fonts, images and styling

--- app/conference/speakers/page.js
+++ app/conference/speakers/page.js
@@ -1,9 +1,11 @@
 import Link from "next/link";
+import styles from "./speaker.module.css";
 
 export default function Page() {
   return (
     <>
       <h1>Welcome to Globomantics Speakers</h1>
+      <p className={styles.speaker}>Speakers page</p>
       <h2>
         <Link href="/conference">Back to Conference</Link>
       </h2>
--- app/home/page.js
+++ app/home/page.js
@@ -1,18 +1,29 @@
 import Link from "next/link";
+import Image from "next/image";
+import ourStoryPic from "../../images/home-image-1.jpg";
+import styles from "./home.module.css";
 
 export default function Page() {
   return (
     <>
-      <h1>Welcome to Home Page</h1>
-      <h2>
-        <Link href="/blog">Blog</Link>
-      </h2>
-      <h2>
-        <Link href="/settings">Settings</Link>
-      </h2>
-      <h2>
-        <Link href="/conference">Conference</Link>
-      </h2>
+      <div className={styles.bgWrap}>
+        <Image
+          src={ourStoryPic}
+          alt="Out story pic"
+          placeholder={"blur"}
+          quality={100}
+          sizes="100vw"
+          fill
+          style={{
+            objectFit: "cover",
+          }}
+        />
+      </div>
+      <h1 className={styles.bgHeader}>Humble beginnings a story of life</h1>
+      <p className={styles.bgText}>
+        How we became the farmers of the future, tilling the technology of
+        tomorrow today.
+      </p>
     </>
   );
 }
