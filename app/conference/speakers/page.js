3e02c13a88c1fee6e99f34166ddfb15f17d904bc
Adhithi Ravichandran <adhithi@redivus.com>
2022-12-10 22:54:30 -0600

    Speakers data fetching

--- app/conference/speakers/page.js
+++ app/conference/speakers/page.js
@@ -1,8 +1,9 @@
-import Link from "next/link";
+import styles from "../conference.module.css";
 
-async function fetchData() {
+// Static site generation by default
+async function fetchSpeakers() {
   const response = await fetch(
-    `https://raw.githubusercontent.com/adhithiravi/Consuming-GraphqL-Apollo/master/api/data/speakers.json`
+    "https://raw.githubusercontent.com/adhithiravi/Consuming-GraphqL-Apollo/master/api/data/speakers.json"
   );
 
   const data = await response.json();
@@ -10,23 +11,20 @@ async function fetchData() {
 }
 
 export default async function Page() {
-  const data = await fetchData();
+  const data = await fetchSpeakers();
 
   return (
-    <>
+    <div className={styles.parentContainer}>
       <div className="self-start whitespace-nowrap rounded-lg bg-gray-700 px-3 py-1 text-sm font-medium tabular-nums text-gray-100">
         Last Rendered: {new Date().toLocaleTimeString()}
       </div>
       <h1>Welcome to Globomantics Speakers</h1>
-      <h2>
-        <Link href="/conference">Back to Conference</Link>
-      </h2>
       {data.speakers.map(({ id, name, bio }) => (
-        <div key={id} style={{ padding: 5 }}>
-          <h3>Speaker: {name}</h3>
-          <h5>Bio: {bio}</h5>
+        <div key={id} className={styles.infoContainer}>
+          <h3 className={styles.titleText}>{name}</h3>
+          <h5 className={styles.descText}>{bio}</h5>
         </div>
       ))}
-    </>
+    </div>
   );
 }
