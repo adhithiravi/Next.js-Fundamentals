b966f716c1ff03a872b4faaa1a56303fba94ca77
Adhithi Ravichandran <adhithi@redivus.com>
2022-12-13 14:17:29 -0600

    Styling update

--- app/conference/speakers/page.js
+++ app/conference/speakers/page.js
@@ -25,7 +25,7 @@ export default async function Page() {
       <h1>Welcome to Globomantics Speakers</h1>
       {data.speakers.map(({ id, name, bio }) => (
         <div key={id} className={styles.infoContainer}>
-          <Link href={`conference/speakers/${name}`}>
+          <Link className={styles.bgLinks} href={`conference/speakers/${name}`}>
             <h3 className={styles.titleText}>{name}</h3>
           </Link>
           <h5 className={styles.descText}>{bio}</h5>
